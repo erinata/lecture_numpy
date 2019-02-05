b08f352c329a1a73e0cdc2e136e59344ef70c2f5
Chungsang Tom Lam <erinata@gmail.com>
2019-02-05 13:29:29 -0500

    initialize

---
+++ try_array.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+data = np.arange(20).reshape(4, 5)
+
+print(data)
+
+print(data.size)
+
+# print(data.size())
+
+#data = np.zeros(4,4)
+data = np.zeros((4,4))
+
+print(data)
+
+
+data = np.arange(20)*3
+print(data)
+
+
+
+
+
+
+
